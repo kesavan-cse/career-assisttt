18509020c225dd4ce909c17e82127423eee49141
kesavan-cse <41139413+kesavan-cse@users.noreply.github.com>
2019-10-11 16:18:15 +0530

    Add files via upload

---
+++ includes/connectionpost.php
@@ -0,0 +1,5 @@
+<?php
+$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
+$con = mysqli_connect("localhost","root","","testing");
+
+?>
---
+++ includes/connectionresume.php
@@ -0,0 +1,5 @@
+<?php
+$connect = new PDO("mysql:host=localhost;dbname=filter", "root", "");
+$con = mysqli_connect("localhost","root","","filter");
+
+?>
